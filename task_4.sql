bc7f4c18e36a40e96b34ae1d14a8eb4fef0a2796
OlaBelal <olabelal1995@gmail.com>
2024-10-20 23:41:04 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -12,5 +12,6 @@ SELECT
 FROM 
     information_schema.columns 
 WHERE 
-    table_schema = 'alx_book_store' AND 
-    table_name = 'Books';
+    TABLE_SCHEMA = 'alx_book_store' AND 
+    TABLE_NAME = 'Books';
+
