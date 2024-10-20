0783050bf2c9b70da2ccf46e8ac20e45487ec039
OlaBelal <olabelal1995@gmail.com>
2024-10-20 23:56:48 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -2,8 +2,15 @@
 USE alx_book_store;
 
 -- Querying the information_schema to get the full description of the books table
-SELECT *
-FROM books
- 
-
-
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA
+FROM 
+    information_schema.columns 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' AND 
+    TABLE_NAME = 'books';
